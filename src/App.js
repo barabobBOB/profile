08b830db730931a2fc264a2a19515b32fe579193
barabobBOB <choi20014830@gmail.com>
2021-08-13 02:43:17 +0900

    main page-1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import react, { Component, component } from 'react';
+import './App.css';
 
 class App extends Component {
-  render () {
+  render() {
     const name = '최세연';
-    return (
-      <div>
-        
+      return (
+      <div className="App">
+        <h3>안녕하세요.<br></br>
+        저만의 끈질김을 바탕으로<br></br>
+        고객의 만족도를 높이는<br></br>
+        개발자, {name}입니다.</h3>
       </div>
-    )
+    );
   }
 }
 
