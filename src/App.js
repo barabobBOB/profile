3b3adffa152e09f6ccc695a7d9afc24c15f0d3bb
barabobBOB <choi20014830@gmail.com>
2021-08-21 23:25:38 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     const name = '최세연';
       return (
-      <div className="App">
+      <div class="App">
         <h3>안녕하세요.<br></br>
         저만의 끈질김을 바탕으로<br></br>
         고객의 만족도를 높이는<br></br>
