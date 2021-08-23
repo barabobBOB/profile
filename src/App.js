ed4e97b3a149d377045dc0a16822cd93865cfc13
baraboBOB <choi20014830@gmail.com>
2021-08-24 04:37:00 +0900

    햄버거

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import react from 'react';
-//import './App.css';
-import Navbar from './Nav/NavBar';
+import React from 'react';
+import Navbar from './Nav/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h3>안녕하세요.<br></br>
+      <h3>안녕하세요<br></br>
       저만의 끈질김을 바탕으로<br></br>
       고객의 만족도를 높이는<br></br>
       개발자, 최세연입니다.</h3>
-      </div>
-    );
+    </div>
+  );
 }
-// class App extends Component {
-//   render() {
-//     const name = '최세연';
-//       return (
-//       <div className="App">
-//         <Navbar />
-//         <h3>안녕하세요.<br></br>
-//         저만의 끈질김을 바탕으로<br></br>
-//         고객의 만족도를 높이는<br></br>
-//         개발자, {name}입니다.</h3>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
